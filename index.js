5de239202ea1f0f712e0b8f84fec8167d4d170ab
unknown <naiduch1981@gmail.com>
2024-06-19 22:44:41 +0530

    made change for Homepage url

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const app= express()
-const PORT=4000;
+
 const dotEnv = require('dotenv')
 const mongoose = require("mongoose")
 const vendorRoutes = require('./routes/vendorRoutes');
@@ -10,21 +9,26 @@ const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
 const path = require('path')
 
+const app= express()
+const PORT = process.env.PORT || 4000;
+
 dotEnv.config();
+app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("mongoose connected"))
-.catch((error) => console.log("error"))
+    .then(() => console.log("MongoDB connected successfully!"))
+    .catch((error) => console.log(error))
 
 app.use(bodyParser.json());
-app.use('/vendor',vendorRoutes);
+app.use('/vendor', vendorRoutes);
 app.use('/firm', firmRoutes)
 app.use('/product', productRoutes);
 app.use('/uploads', express.static('uploads'));
 
-app.listen(PORT,() => {
-    console.log(`server running on port ${PORT}`);
-})
-app.use('/home',(req,res) => {
-    res.send(`server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`server started and running at ${PORT}`);
+});
+
+app.use('/', (req, res) => {
+    res.send("<h1> Welcome to Restro");
 })
